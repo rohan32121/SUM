3dd3c91a796b3d856d27db6f17fa50562652b2c6
ROHAN KARKI <124191920+rohan32121@users.noreply.github.com>
2023-06-26 12:24:17 +0530

    Add files via upload

---
+++ SUM ASINGMENT/3.js
@@ -0,0 +1,12 @@
+// 1. Write a function that takes two numbers as arguments and returns their sum. Declare a variable inside the
+// function using the var keyword and log its value to the console before it is assigned a value to demonstrate
+// variable hoisting.
+
+function addTwoNumbers(num1, num2) {
+    console.log(sum);
+    var sum;
+    sum = num1 + num2;
+    return sum
+}
+
+console.log(addTwoNumbers(5, 5));
---
+++ SUM ASINGMENT/5.js
@@ -0,0 +1,6 @@
+// 5. Declare a variable using let inside a block scope and attempt to log its value to the console before it is
+// assigned a value to demonstrate the temporal dead zone.
+
+console.log(x);
+let x = "HELLOW";
+console.log(x);
